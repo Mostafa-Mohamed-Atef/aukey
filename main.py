8ea712ebbd017137b95f4c2859b27c82e373c4f5
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-08-20 22:08:59 +0300

    searching and matching key inputs

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import subprocess
 import evdev
 
-device_path = '/dev/input/event0'  
 
+#for listening to keyboard
+device_path = '/dev/input/event0'  
 try:
     device = evdev.InputDevice(device_path) #device python will listen to
 except FileNotFoundError:
     print(f"Device not found: {device_path}")
     exit(1)
 
-path = "/home/mostafa/programming"
 print("Listening...")
+path = {'333356':'/home/mostafa/programming', '181856':'/home/mostafa/Downloads'}
+
+
+
 x= ""
+# path = "/home/mostafa/programming"
 for event in device.read_loop(): #for listening for event
     if event.type == evdev.ecodes.EV_KEY:
         key_event = evdev.categorize(event) # Only print on key press, not release
         if key_event.event.value == 0:
             x += str(key_event.event.code) #alt+f+f
             print(x)
-        if x == '333356':
-            subprocess.run(["xdg-open", path])
+        match = next((key for key in path if key in x), None)
+        if match:
+            print('the file is opening please wait...')
+            subprocess.run(["xdg-open", path[match]])
             break
