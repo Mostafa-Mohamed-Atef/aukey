a944f8473a51b96ea416abb87b62d062b2ca5155
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-08-20 22:42:15 +0300

    created a json file for all paths

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import subprocess
 import evdev
-
+import json
 
 #for listening to keyboard
-device_path = '/dev/input/event0'  
+with open('paths.json', 'r') as file:
+    data = json.load(file)
+
+device_path = data.get("keyboard")
 try:
     device = evdev.InputDevice(device_path) #device python will listen to
 except FileNotFoundError:
@@ -11,20 +14,18 @@ except FileNotFoundError:
     exit(1)
 
 print("Listening...")
-path = {'333356':'/home/mostafa/programming', '181856':'/home/mostafa/Downloads'}
 
 
 
 x= ""
-# path = "/home/mostafa/programming"
 for event in device.read_loop(): #for listening for event
     if event.type == evdev.ecodes.EV_KEY:
         key_event = evdev.categorize(event) # Only print on key press, not release
         if key_event.event.value == 0:
-            x += str(key_event.event.code) #alt+f+f
+            x += str(key_event.event.code) #alt+f+f , alt+e+e for Downloads
             print(x)
-        match = next((key for key in path if key in x), None)
+        match = next((key for key in data if key in x), None)
         if match:
             print('the file is opening please wait...')
-            subprocess.run(["xdg-open", path[match]])
+            subprocess.run(["xdg-open", data[match]])
             break
