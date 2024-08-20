7be19e2ded21d6dceca684a326cb8560f1922bd2
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-08-20 17:46:44 +0300

    opens a folder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
+import subprocess
 import evdev
-# import pyautogui 
 
 device_path = '/dev/input/event0'  
 
@@ -10,7 +9,15 @@ except FileNotFoundError:
     print(f"Device not found: {device_path}")
     exit(1)
 
+path = "/home/mostafa/programming"
+print("Listening...")
+x= ""
 for event in device.read_loop(): #for listening for event
     if event.type == evdev.ecodes.EV_KEY:
         key_event = evdev.categorize(event) # Only print on key press, not release
-        print(key_event.event.code)
+        if key_event.event.value == 0:
+            x += str(key_event.event.code) #alt+f+f
+            print(x)
+        if x == '333356':
+            subprocess.run(["xdg-open", path])
+            break
